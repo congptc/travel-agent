de623042c9c5b294f16e01e31370084870d1d5dd
Pham Thanh Cong <congptc@gmail.com>
2026-01-22 00:12:00 +0700

    Thay doi requirement files

--- main.py
+++ main.py
@@ -8,17 +8,8 @@ from noti.noti_factory import NotiFactory
 
 if __name__ == "__main__":
     
-    places_provider = PlacesProviderFactory.create("mock")
-    agent_ai = AgentFactory.create("mock")
-    noti = NotiFactory.create("mock")
-
-    raw_places = places_provider.search_nearby(
-        lat=CONFIG["location"][0],
-        lng=CONFIG["location"][1],
-        radius_m=CONFIG["radius"]
-    )
-    if raw_places:
-        summary = agent_ai.ai_summarize(raw_places[:10])
-        noti.send(f"📅 **KẾ HOẠCH CUỐI TUẦN CHO BẠN**\n{summary}")
-    else:
-        noti.send("Hôm nay không tìm thấy địa điểm nào phù hợp tiêu chí rồi!")
+    #places_provider = PlacesProviderFactory.create("mock")
+    agent_ai = AgentFactory.create("gemini")
+    noti = NotiFactory.create("telegram")
+    summary = agent_ai.ai_summarize("")
+    noti.send(f"📅 **KẾ HOẠCH CUỐI TUẦN CHO BẠN**\n{summary}")
