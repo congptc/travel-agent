9aaaf467acc82f86c132340518525077cd5f0829
Pham Thanh Cong <congptc@gmail.com>
2026-01-22 20:50:38 +0700

    sua lai dung html thay markdown

--- noti/telegram_noti.py
+++ noti/telegram_noti.py
@@ -18,7 +18,7 @@ class TelegramNoti(NotiChanel):
 
     def send(self, summary):
         url = f"https://api.telegram.org/bot{self.telegram_token}/sendMessage"
-        payload = {"chat_id": self.telegram_chat_id, "text": summary}
+        payload = {"chat_id": self.telegram_chat_id, "text": summary,"parse_mode": "HTML"}
         print(f"Địa chỉ telegram: {url}")
         print(f"Telegram có chạy đến đây ko: {summary}")
         try:
